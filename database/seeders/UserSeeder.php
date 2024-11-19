fda97d2a2b854ad66051fcf19b8ca60927ba6a37
Yasin Cengiz Coskun <y.cengiz.coskun@gmail.com>
2024-11-20 02:57:22 +0300

    UserSeeder created

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserStatus;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(['email' => 'y.cengiz.coskun@gmail.com'], [
+            'name' => 'Yasin Cengiz Coskunn',
+            'email' => 'y.cengiz.coskun@gmail.com',
+            'password' => Hash::make('yIck5bKzhtX1b1Lp'),
+        ]);
+    }
+}
